88cb519e00ceea06236e9a688f2c0c26424fc9c1
ndvo <nelson@ocastudios.com>
2020-11-05 17:59:43 -0300

    test: test against mocked cf worker site

--- index.test.cjs
+++ index.test.cjs
@@ -13,9 +13,9 @@ const options = {
 }
 
 const cases = {
-  basic: new URL('http://localhost:8787/sandbox/foxy/'),
-  restricted: new URL('http://localhost:8787/sandbox/foxy/restricted.html'),
-  login: new URL('http://localhost:8787/login/'),
+  basic: new URL('http://localhost:8787/basic'),
+  restricted: new URL('http://localhost:8787/restricted.html'),
+  login: new URL('http://localhost:8787/customer-portal/'),
 }
 
 describe('Protect restricted content', () => {
@@ -37,8 +37,11 @@ describe('Protect restricted content', () => {
 
     it ('Does not redirect anonymous users away from the login page', async () => {
       const response = await fetch(cases.login.toString(), options);
+      expect(response.status).not.to.equal(302);
       const cookie = response.headers.get('set-cookie') ;
-      expect(cookie).not.to.contain('fx.cf.guard.destination');
+      if (cookie) {
+        expect(cookie).not.to.contain('fx.cf.guard.destination');
+      }
     });
   });
 
@@ -47,8 +50,13 @@ describe('Protect restricted content', () => {
     const headers = { cookie: `fx.customer.jwt=${token}` }
     const loggedIn = { ...options, headers }
     const response = await fetch(cases.basic.toString(), loggedIn);
+    expect(response.status).not.to.equal(302);
+    const location = response.headers.get('location');
+    expect(location).not.to.exist;
     const cookie = response.headers.get('set-cookie') ;
-    expect(cookie).not.to.contain('fx.cf.guard.destination');
+    if (cookie) {
+      expect(cookie).not.to.contain('fx.cf.guard.destination');
+    }
   });
 
   it ('Removes tags identified as restricted if the user is anonymous', async () => {
